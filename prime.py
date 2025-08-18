a7238cee73d3b9b10ff323d34f13daafc3811309
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-18 23:13:23 +0530

    prime number implementation

---
+++ prime.py
@@ -0,0 +1,17 @@
+import math
+
+
+def prime(n):
+    if n >=2 :
+        for i in range(2,int(math.sqrt(n)+1)):
+            if n % i == 0:
+                return False
+            else:
+                continue
+        return True
+    else:
+        return False
+
+
+for i in range(1,10):
+    print(prime(i) , i)
