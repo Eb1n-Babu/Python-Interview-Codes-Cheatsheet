32d8fe3ea16271f14b5ce159f2d13c87ef4bbe1f
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-18 19:37:36 +0530

    programe to list to string

---
+++ list_to_string.py
@@ -0,0 +1,10 @@
+def list_to_string(list_):
+    print()
+    for element in list_:
+        x = str(element)
+        print(x , end='')
+
+lis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12]
+lis_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12]
+list_to_string(lis)
+list_to_string(lis_1)
