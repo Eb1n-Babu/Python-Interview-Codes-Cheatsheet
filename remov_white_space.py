dabc5fade46033113d836ad581802dcbff91cfc8
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-18 22:33:48 +0530

    done some updations

--- remov_white_space.py
+++ remov_white_space.py
@@ -1,5 +1,13 @@
+import re
+
+
 def remove_whitespace(s):
     return s.replace(" ", "")
 
+def remove_whitespaces1(s):
+    r = re.sub(r" ", "", s)
+    return r
+
 
 print(remove_whitespace("diuuhdudiudueuj     "))
+print(remove_whitespaces1("diuuh  dudiudueuj     "))
