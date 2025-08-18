4fe475a2aa5a20e66e394645b4d535ff2a4ce11d
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-18 18:12:28 +0530

    for find the smallest number in a list

---
+++ min_in_list.py
@@ -0,0 +1,9 @@
+def min_in_list(lst):
+    sorted_lst = sorted(lst)
+    min_value = sorted_lst[0]
+    return min_value
+
+
+
+list1 = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(min_in_list(list1))
