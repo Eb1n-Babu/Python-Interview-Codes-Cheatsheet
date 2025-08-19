b0040e9fa6f3cac3ee9bb48931736777f6bc70df
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-19 11:19:18 +0530

    global function

---
+++ global_function.py
@@ -0,0 +1,10 @@
+# Global function to calculate area of a rectangle
+def calculate_area(length, width):
+    return length * width
+
+# Using the global function
+area1 = calculate_area(5, 3)
+area2 = calculate_area(10, 2)
+
+print(f"Area 1: {area1}")
+print(f"Area 2: {area2}")
