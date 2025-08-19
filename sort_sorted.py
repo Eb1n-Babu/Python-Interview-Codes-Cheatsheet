9897b56b8e6580df851e5689ff35d13134d6c28d
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-19 13:13:13 +0530

    sort and sorting

---
+++ sort_sorted.py
@@ -0,0 +1,16 @@
+def sort(list_1):
+    list_1.sort()
+    return list_1
+
+def Sorted(list_1):
+    x = sorted(list_1)
+    return x
+
+list_1 = [1,2,3,4,6,8,9,5]
+list_2 = [9,6,5,4,3,2,1]
+
+print(sort(list_1))
+print(Sorted(list_1))
+print(Sorted(list_2))
+sorted(list_1)
+
