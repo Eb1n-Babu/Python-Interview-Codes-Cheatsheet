3968ba3c67e17c81d7eafa748f50227ebfcd6129
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-19 11:25:38 +0530

    type conversion

---
+++ type_conversion_basics.py
@@ -0,0 +1,14 @@
+list_1 = "1234"
+l = 123
+x = int(list_1)
+y = float(list_1)
+a = complex(list_1)
+b = str(l)
+c = bool('true')
+d = bin(l)
+e = oct(l)
+f = hex(l)
+g = ord('c')
+h = chr(89)
+
+print(x,y,a,b,c,d,e,f,g,h,)
